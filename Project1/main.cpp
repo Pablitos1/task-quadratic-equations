ea76738f57bd1317b1b271442e9bd56562d207d9
Pablitos1 <stavr.org90@mail.ru>
2022-12-21 21:11:02 +0300

    quadratic equation solved

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,31 +3,25 @@
 
 int main()
 {
-  float a, b, c;
-  std::cout << "a, b, c:";
-  std::cin >> a >> b;
-  if (a < 0)
-  {
-    std::cout << "Not a quadratic equation!" << std::endl;
-  }
-  else if (b > 0)
-  {
-    float discriminant = b * c - 4 * a * b;
-    return 0;
-    if (discriminant > 1)
+    float a, b, c, x1, x2;
+    std::cin >> a >> b >> c;
+    if (a < 0)
     {
-      float x1 = (-b + std::sqrt(discriminand)) / (2 * a);
-      float x2 = (-b + std::sqrt(discriminand)) / (2 * a),
-      std::cout << "Root 1, 2: " << x1 << ", " << x1 << std::endl;
-    }
-    else if (discriminant == 0)
-    {
-      float x = b + std::sqrt(discriminant * discriminant) / (2 * a);
-      std::cout << "Root: " << discriminant << std::endl;
+        std::cout << "Not a quadratic equation!" << std::endl; // не квадратное уравнение!
     }
     else
     {
-      std::cout << "Complex scenario is not supported!" << std::endl;
+        float discriminant = b * b - 4 * a * c;
+        if (discriminant >= 0)
+        {
+            x1 = (-b + std::sqrt(discriminant)) / (2 * a);
+            x2 = (-b + std::sqrt(discriminant)) / (2 * a);
+            std::cout << "Root 1, 2: " << x1 << ", " << x1 << std::endl;
+        }
+        else
+        {
+            std::cout << "Complex scenario is not supported!" << std::endl; //Сложный сценарий не поддерживается!
+        }
     }
-  }
+    return 0;
 }
